afff54963ea9e2aa5f554ad0d73c2659c10be78d
Sudheer <sudheer.linx@gmail.com>
2022-05-07 16:27:42 +0530

    Create scattering_multipath_channel.py

---
+++ Noise_modelling/white_noise/scattering_multipath_channel.py
@@ -0,0 +1,12 @@
+https://www.gaussianwaves.com/2014/07/statistical-characteristics-of-multipath-channels-scattering-function/
+ 
+
+Wireless channel is of time-varying nature in which the parameters randomly change with respect to time. 
+Wireless channel is very harsh when compared to AWGN channel model 
+
+
+ modelling wireless channel through multipath channel models
+----------------------------------------------------------------
+Wide Sense Stationary channel,
+uncorrelated scattering channel,
+wide sense stationary uncorrelated scattering channel

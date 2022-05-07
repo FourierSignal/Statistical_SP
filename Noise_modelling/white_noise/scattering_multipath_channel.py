1a1405a7e3ce863193cf7927e07abfa56cc3a025
Sudheer <sudheer.linx@gmail.com>
2022-05-07 16:32:32 +0530

    Update scattering_multipath_channel.py

--- Noise_modelling/white_noise/scattering_multipath_channel.py
+++ Noise_modelling/white_noise/scattering_multipath_channel.py
@@ -4,9 +4,24 @@ https://www.gaussianwaves.com/2014/07/statistical-characteristics-of-multipath-c
 Wireless channel is of time-varying nature in which the parameters randomly change with respect to time. 
 Wireless channel is very harsh when compared to AWGN channel model 
 
+various statistical characteristics of a multipath wireless channel
 
  modelling wireless channel through multipath channel models
 ----------------------------------------------------------------
 Wide Sense Stationary channel,
 uncorrelated scattering channel,
 wide sense stationary uncorrelated scattering channel
+
+
+
+Complex baseband mutipath channel model:
+ 
+ In a multipath channel, multiple copies of a signal travel different paths with different propagation delays \tau 
+ and are received at the receiver at different phase angles and strengths. 
+ These rays add constructively or destructively at the receiver front end, thereby giving rise to rapid fluctuations in the channel. 
+ 
+ 
+ The multipath channel can be viewed as a linear time variant system where the parameters change randomly with respect to time. The channel impulse response is a two dimensional random variable – h(t, \tau) that is a function of two parameters –  instantaneous time t and the propagation delay \tau.
+ 
+ The  channel is expressed as a set of random complex gains at a given time t and the propagation delay \tau. The output of the channel y(t) can be expressed as the convolution of the complex channel impulse response h(t, \tau) and the input x(t)
+ 
